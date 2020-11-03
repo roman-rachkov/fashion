9dd81148ba02741b3ad60280bc564d30aee10176
Rachkov Roman <rachkoff.roman@yandex.ru>
2020-11-03 22:28:33 +1000

    mobile menu add

--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,8 @@
 jQuery(document).ready(function($){
-
+    $('.mobile-toggle').click(function (e){
+        console.log(1);
+        e.preventDefault();
+        let target = $($(this).data('target'));
+        target.toggleClass('active');
+    });
 });
